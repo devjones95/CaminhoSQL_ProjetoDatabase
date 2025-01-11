60a56f82cada24238ab0f37aadb64a780b6fc14c
João Pedro Assis <126593707+devjones95@users.noreply.github.com>
2025-01-11 08:59:00 -0300

    Update Query FInal.sql

--- Query Final/Query FInal.sql
+++ Query Final/Query FInal.sql
@@ -2,14 +2,14 @@ USE alucar;
 -- criando as tables da database
 
 CREATE TABLE marcas (
-	id INT NOT NULL AUTO_INCREMENT, -- a coluna será do tipo inteiro, não poderá ser nulo e auto incremento de 1 em 1
+    id INT NOT NULL AUTO_INCREMENT, -- a coluna será do tipo inteiro, não poderá ser nulo e auto incremento de 1 em 1
     nome_marca VARCHAR(255) NOT NULL, -- tipo string com o tamanho máximo de caracteres de 255 e não poderá ser nulo
     origem  VARCHAR(255) NOT NULL,
     PRIMARY KEY (id) -- será a primary key da table, no caso o id
 );
 
 CREATE TABLE inventario (
-	id INT NOT NULL AUTO_INCREMENT,
+   id INT NOT NULL AUTO_INCREMENT,
     modelo VARCHAR(255) NOT NULL,
     transmissao VARCHAR(255) NOT NULL,
     motor VARCHAR(255) NOT NULL,
@@ -20,10 +20,10 @@ CREATE TABLE inventario (
 );
 
 CREATE TABLE clientes (
-	id INT NOT NULL AUTO_INCREMENT,
+   id INT NOT NULL AUTO_INCREMENT,
     nome VARCHAR(255) NOT NULL,
     sobrenome VARCHAR(255) NOT NULL,
-	endereco VARCHAR(255) NOT NULL,
+    endereco VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
 
